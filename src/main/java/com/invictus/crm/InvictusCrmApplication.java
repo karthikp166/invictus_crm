eb838931a5b14d01bd3c309a8c86f5a304c09293
Prakash1, Karthik <Karthik.Prakash1@in.tesco.com>
2019-05-29 16:08:04 +0530

    base code

---
+++ src/main/java/com/invictus/crm/InvictusCrmApplication.java
@@ -0,0 +1,16 @@
+package com.invictus.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
+@SpringBootApplication
+@EnableRetry
+public class InvictusCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvictusCrmApplication.class, args);
+
+	}
+
+}
